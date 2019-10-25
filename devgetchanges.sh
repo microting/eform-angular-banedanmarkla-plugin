72b1cf465d2dd2d9dbea959911b0abfc14d86210
MathiasHusum <mathiashusumn@gmail.com>
2019-10-25 08:03:48 +0200

    Create devgetchanges.sh

---
+++ devgetchanges.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~
+
+if [ -d "Documents/workspace/microting/eform-angular-banedanmarkla-plugin/eform-client/src/app/plugins/modules/bane-danmark-la-pn" ]; then
+	rm -fR Documents/workspace/microting/eform-angular-banedanmarkla-plugin/eform-client/src/app/plugins/modules/bane-danmark-la-pn
+fi
+
+cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/bane-danmark-la-pn Documents/workspace/microting/eform-angular-banedanmarkla-plugin/eform-client/src/app/plugins/modules/bane-danmark-la-pn
+
+if [ -d "Documents/workspace/microting/eform-angular-banedanmarkla-plugin/eFormAPI/Plugins/BaneDanmarkLa.Pn" ]; then
+	rm -fR Documents/workspace/microting/eform-angular-banedanmarkla-plugin/eFormAPI/Plugins/BaneDanmarkLa.Pn
+fi
+
+cp -av Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/BaneDanmarkLa.Pn Documents/workspace/microting/eform-angular-banedanmarkla-plugin/eFormAPI/Plugins/BaneDanmarkLa.Pn
